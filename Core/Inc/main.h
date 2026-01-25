08fee60832ce07e8fb69f6e7896661bb3c9f40f5
Jacob Baumel <jacob.a.baumel@gmail.com>
2026-01-24 22:37:10 -0600

    Initial IOC generation

---
+++ Core/Inc/main.h
@@ -0,0 +1,85 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define SCH0_Pin GPIO_PIN_2
+#define SCH0_GPIO_Port GPIOE
+#define SCH4_Pin GPIO_PIN_3
+#define SCH4_GPIO_Port GPIOE
+#define SCH1_Pin GPIO_PIN_4
+#define SCH1_GPIO_Port GPIOE
+#define SCH2_Pin GPIO_PIN_5
+#define SCH2_GPIO_Port GPIOE
+#define SCH3_Pin GPIO_PIN_6
+#define SCH3_GPIO_Port GPIOE
+#define SCH6_Pin GPIO_PIN_7
+#define SCH6_GPIO_Port GPIOF
+#define SCH5_Pin GPIO_PIN_8
+#define SCH5_GPIO_Port GPIOF
+#define SCH7_Pin GPIO_PIN_9
+#define SCH7_GPIO_Port GPIOF
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
