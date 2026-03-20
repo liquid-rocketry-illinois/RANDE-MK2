583a3a5f75b2a11df355b22e3f11313df3a74646
Jacob Baumel <jacob.a.baumel@gmail.com>
2026-03-19 23:49:20 -0500

    Added second load cell

--- Core/Src/LoadCells.cpp
+++ Core/Src/LoadCells.cpp
@@ -67,7 +67,7 @@ namespace LoadCells {
                 // Reset counter
                 timer->CNT = 0;
 
-                // Enable output
+                // Enable the moe uwu
                 timer->BDTR |= TIM_BDTR_MOE;
 
                 // Make sure we are still on channel 1, compile error if user changes this
@@ -98,23 +98,84 @@ namespace LoadCells {
             }
         } // namespace C1
 
+        namespace C2 {
+            TIM_TypeDef* const timer = TIM1;
+            uint32_t const timch = TIM_CHANNEL_1;
+
+            GPIO_TypeDef* const clkport = GPIOE;
+            uint16_t const clkpin = GPIO_PIN_9;
+
+            GPIO_TypeDef* dinport = CELL2_DIN_GPIO_Port;
+            uint16_t const dinpin = CELL2_DIN_Pin;
+
+            uint32_t inProgressReading = 0;
+            int32_t rawLatestReading = 0;
+            uint32_t pulseCount = 0;
+
+            float offset = 0;
+            float scale = 0;
+
+            float latestReading;
+
+            void initCell() {
+                __HAL_RCC_GPIOE_CLK_ENABLE();
+                __HAL_RCC_GPIOG_CLK_ENABLE();
+                __HAL_RCC_TIM1_CLK_ENABLE();
+
+                GPIO_InitTypeDef init;
+                init.Pin = clkpin;
+                init.Mode = GPIO_MODE_AF_PP;
+                init.Pull = GPIO_NOPULL;
+                init.Alternate = GPIO_AF1_TIM1;
+                HAL_GPIO_Init(clkport, &init);
+
+                timer->CR1 &= ~TIM_CR1_CEN;
+                timer->CR1 &= ~(TIM_CR1_ARPE | TIM_CR1_CMS | TIM_CR1_DIR);
+                timer->CR1 |= TIM_CR1_OPM;
+                timer->PSC = (240000000 / 10000000) - 1;
+                timer->ARR = 20;
+                timer->RCR = 24;
+                timer->CNT = 0;
+                timer->BDTR |= TIM_BDTR_MOE;
+
+                // Make sure we are still on channel 1, compile error if user changes this
+                static_assert(timch == TIM_CHANNEL_1, "Change registers to change timer channels");
+
+                timer->CCER &= ~(TIM_CCER_CC1E | TIM_CCER_CC1P);
+                timer->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2;
+                timer->CCR1 = 10;
+                timer->CCER |= TIM_CCER_CC1E | TIM_CCER_CC1P;
+                timer->DIER |= TIM_DIER_CC1IE;
+
+                HAL_NVIC_SetPriority(TIM1_CC_IRQn, 0, 0);
+                HAL_NVIC_EnableIRQ(TIM1_CC_IRQn);
+                HAL_NVIC_SetPriority(TIM1_UP_IRQn, 0, 0);
+                HAL_NVIC_EnableIRQ(TIM1_UP_IRQn);
+            }
+        } // namespace C2
+
         uint32_t lastLogged = 0;
     } // namespace
 
-    void init() { C1::initCell(); }
+    void init() {
+        C1::initCell();
+        C2::initCell();
+    }
 
     void yield() {
         C1::latestReading = C1::scale * (static_cast<float>(C1::rawLatestReading) + C1::offset);
+        C2::latestReading = C2::scale * (static_cast<float>(C2::rawLatestReading) + C2::offset);
 
         if(RCP::getDataStreaming() && HAL_GetTick() - lastLogged > 10) {
             lastLogged = HAL_GetTick();
             RCP::sendOneFloat(RCP_DEVCLASS_LOAD_CELL, 0, readCell(0));
+            RCP::sendOneFloat(RCP_DEVCLASS_LOAD_CELL, 1, readCell(1));
         }
     }
 
     float readCell(uint8_t id) {
         if(id == 0) return C1::latestReading;
-        if(id == 1) return 1;
+        if(id == 1) return C2::latestReading;
         return 0;
     }
 
@@ -148,37 +209,63 @@ extern "C" void EXTI15_10_IRQHandler(void) {
         // Start the PWM
         C1::timer->CR1 |= TIM_CR1_CEN;
     }
+
+    else if(__HAL_GPIO_EXTI_GET_IT(C2::dinpin)) {
+        __HAL_GPIO_EXTI_CLEAR_IT(C2::dinpin);
+        LL_EXTI_DisableEvent_0_31(C2::dinpin);
+        C2::pulseCount = 0;
+        C2::inProgressReading = 0;
+        C2::timer->CNT = 0;
+        C2::timer->CR1 |= TIM_CR1_CEN;
+    }
 }
 
 // This interrupt detects when the 25 pulses have finished
-extern "C" void TIM8_UP_IRQHandler(void) {
+extern "C" void TIM8_UP_TIM13_IRQHandler(void) {
     using namespace LoadCells;
     // Acknowledge the interrupt
     C1::timer->SR &= ~TIM_SR_UIF;
     // Copy the raw reading over to the latest reading variable that can be used for calculating the measurement
     // This copy performs the sign extension for 2s complement 32-bit from unsigned 24-bit
-    C1::rawLatestReading =
-        static_cast<int32_t>((0xFF000000 & (C1::inProgressReading & 0x00800000)) | // Sign extended portion
-                             (C1::inProgressReading & 0x00FFFFFF)); // Value
+    if(C1::inProgressReading & 0x00800000) C1::inProgressReading += 0xFF000000;
+    C1::rawLatestReading = static_cast<int32_t>(C1::inProgressReading);
 
     // Re-enable the DIN interrupt
     LL_EXTI_EnableIT_0_31(C1::dinpin);
 }
 
+extern "C" void TIM1_UP_IRQHandler(void) {
+    using namespace LoadCells;
+    C2::timer->SR &= ~TIM_SR_UIF;
+    if(C2::inProgressReading & 0x00800000) C2::inProgressReading += 0xFF000000;
+    C1::rawLatestReading = static_cast<int32_t>(C2::inProgressReading);
+    LL_EXTI_EnableEvent_0_31(C2::dinpin);
+}
+
 // This interrupt is fired whenever the timers CCR and CNT registers are equal, aka at half way through a pulse. If
 //     we are still shifting in data (in the first 24 pulses), read the GPIO and accumulate/shift the in progress
 //     variable.
-extern "C" void TIM8_UP_TIM13_IRQHandler(void) {
+extern "C" void TIM8_CC_IRQHandler(void) {
     using namespace LoadCells;
     // Acknowledge the interrupt
     C1::timer->SR &= ~TIM_SR_CC1IF;
     // Check if we are still in the data phase
     if(C1::pulseCount++ < 24) {
         // Read DIN, if 1 add 1 to inProgressReading
-        static_assert(C1::dinpin == 0x8000, "Must change right shift value if pin changes");
+        static_assert(C1::dinpin == 1 << 15, "Must change right shift value if pin changes");
         C1::inProgressReading += (C1::dinport->IDR & C1::dinpin) >> 15;
 
         // On every clock, left shift the value
         C1::inProgressReading <<= 1;
+    }
+}
+
+extern "C" void TIM1_CC_IRQHandler(void) {
+    using namespace LoadCells;
+    C2::timer->SR &= ~TIM_SR_CC1IF;
+    if(C2::pulseCount++ < 24) {
+        static_assert(C2::dinpin == 1 << 12, "Must change right shift value if pin changes");
+        C2::inProgressReading += (C2::dinport->IDR & C2::dinpin) >> 12;
+        C2::inProgressReading <<= 1;
     }
 }
